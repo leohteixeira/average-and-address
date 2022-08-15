92486392d932a13f0367614cdcf3a88aba061647
DevLeuzera <le.tex@hotmail.com>
2022-08-14 21:47:13 -0300

    feat: add Address model

---
+++ src/domain/models/address.ts
@@ -0,0 +1,15 @@
+export namespace Address {
+  export interface Model {
+    cep?: string
+    logradouro?: string
+    complemento?: string
+    bairro?: string
+    localidade?: string
+    uf?: string
+    ibge?: string
+    gia?: string
+    ddd?: string
+    siafi?: string
+    erro?: string
+  }
+}
